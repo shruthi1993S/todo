99df0f302bc4e78978556305bd91ab03fd0702a3
shruthi1993s <shruthiji06@gmail.com>
2024-06-20 22:37:07 +0530

    todo task

---
+++ src/APP.jsx
@@ -0,0 +1,14 @@
+import TodoApp from './TodoApp'
+import React from 'react'
+
+function App() {
+  return (
+<div>
+      <h1 className='text-center'>To-do Application</h1>
+      <TodoApp></TodoApp>
+    </div>
+    
+  )
+}
+
+export default App
